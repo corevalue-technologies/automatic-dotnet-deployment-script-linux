5ce8bafce40c35e8a8597197841228b9b0ef2dda
Ronak Dhoot <ipg007.50.iiitm@gmail.com>
2019-12-31 15:35:20 +0530

    Correction suggestion

--- dotnetdeployment.sh
+++ dotnetdeployment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+FILENAME=$1
 wget -q https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
@@ -21,4 +22,4 @@ sudo chmod 777 /var/www/html/
 sudo mv /home/publish/* /var/www/html/
 sudo chmod -R 777 /var/www/html/
 cd /var/www/html/
-dotnet <FileName>.dll &
+dotnet $FILENAME &
